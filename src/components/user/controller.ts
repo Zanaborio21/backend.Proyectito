cec4ac0984d8b575bada9b41baa3931546de0805
zanaborio <zanaborio21@gmail.com>
2023-03-01 01:46:53 -0500

    segundo commit

--- src/components/user/controller.ts
+++ src/components/user/controller.ts
@@ -1,29 +1,55 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
-import { hash } from "bcrypt";
+import { compare, hash } from "bcrypt";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 const prisma = new PrismaClient();
+const jwtSecret = process.env.JWT_SECRET || "secret";
 
-export const crearUsuario = async function createUser(req: Request, res: Response) {
+export const login = async function login(req: Request, res: Response): Promise<void> {
   const { nombre, password } = req.body;
 
-  const existingUser = await prisma.user.findUnique({
+  const user = await prisma.user.findUnique({
     where: { nombre },
   });
 
-  if (existingUser) {
-    return res.status(400).json({ message: "El usuario ya existe" });
+  if (!user) {
+    res.status(401).json({ message: "Usuario o contraseña incorrectos" });
+    return;
   }
 
-  const hashPassword = await hash(password, 10);
+  const isPasswordValid = await compare(password, user.password);
 
-  const user = await prisma.user.create({
-    data: {
-      nombre,
-      password: hashPassword,
-    },
-  });
+  if (!isPasswordValid) {
+    res.status(401).json({ message: "Usuario o contraseña incorrectos" });
+    return;
+  }
 
-  res.status(201).json(user);
-}
+  const token = jwt.sign({ id: user.id }, jwtSecret, { expiresIn: "1h" });
 
+  res.status(200).json({ token });
+};
+
+export const verifyToken = async function verifyToken(
+  req: Request,
+  res: Response,
+  next: () => void
+): Promise<void> {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    res.status(401).json({ message: "Token no proporcionado" });
+    return;
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decodedToken = jwt.verify(token, jwtSecret) as JwtPayload;
+    req.body.userId = decodedToken.id;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Token inválido" });
+    return;
+  }
+};
